fc4dfdb3ce281471fdeb0d5be19f66bbd9e52d43
Bram <bram.decuypere@gmail.com>
2023-02-03 13:15:01 +0100

    don't forget to import migrations

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,6 @@
 import { Meteor } from "meteor/meteor";
-import { Accounts } from "meteor/accounts-base";
+
+import "/server/migrations";
 import "/imports/api/recipes/recipes.methods";
 import "/imports/api/recipes/recipes.publications";
 import "/imports/api/users/users.methods";
