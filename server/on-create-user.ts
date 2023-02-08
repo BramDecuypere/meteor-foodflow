c932af7a23bc0ceb824ff72c995d248390fe874b
Bram <bram.decuypere@gmail.com>
2023-02-08 17:45:01 +0100

    add clarification

--- server/on-create-user.ts
+++ server/on-create-user.ts
@@ -1,7 +1,10 @@
 import { Accounts } from "meteor/accounts-base";
+import { Meteor } from "meteor/meteor";
 
 Accounts.onCreateUser((options, user) => {
-  const customizedUser = {
+  // TODO: CAREFUL: We add anything that is added to the user
+  // this can become a problem once we allow users to be created by signup
+  const customizedUser: Meteor.User = {
     ...user,
     ...options,
   };
