8f6b36fe7b3619c5281cc72a2688a5f58b242496
Bram <bram.decuypere@gmail.com>
2023-02-15 10:30:46 +0100

    add empty navigation

--- constants/navigation.ts
+++ constants/navigation.ts
@@ -5,14 +5,22 @@ import {
 } from "@heroicons/react/24/outline";
 
 export const navigation = [
-  { name: "My Recipes", href: "/dashboard", icon: HomeIcon, current: false },
   {
+    id: "dashboard",
+    name: "My Recipes",
+    href: "/dashboard",
+    icon: HomeIcon,
+    current: false,
+  },
+  {
+    id: "active-list",
     name: "Active Groceries",
     href: "/groceries",
     icon: ShoppingCartIcon,
     current: false,
   },
   {
+    id: "settings",
     name: "Settings",
     href: "/settings",
     icon: Cog6ToothIcon,
--- imports/ui/pages/groceries.tsx
+++ imports/ui/pages/groceries.tsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import TextToggle from "../atoms/TextToggle";
 import ActiveMenuList from "../organisms/active-menu-list";
 import GroceriesList from "../organisms/groceries-list/groceries-list";
+import ActiveListHook from "../hooks/active-list.hook";
+import { navigation } from "/constants/navigation";
+import Button from "../atoms/Button";
+import { useNavigate } from "react-router-dom";
 
 export enum GroceriesListStates {
   MENU = "My Active Menu",
@@ -10,9 +14,48 @@ export enum GroceriesListStates {
 }
 
 const Groceries = () => {
+  const activeList = ActiveListHook();
+  const navigate = useNavigate();
+
   const [selectedView, setSelectedView] = useState<GroceriesListStates>(
     GroceriesListStates.GROCERIES_LIST
   );
+
+  const getNavigation = () => {
+    const navigateItem = navigation.find((val) => val.id === "dashboard");
+
+    if (!navigateItem) {
+      return null;
+    }
+
+    return navigateItem;
+  };
+
+  if (activeList.recipes.length === 0) {
+    return (
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        {/* Replace with your content */}
+        <div className="px-4 py-8 sm:px-0">
+          <div className="h-96 rounded-lg border-4 border-dashed border-gray-200 flex justify-center items-center">
+            {getNavigation() && (
+              <Button
+                onClick={() => {
+                  const item = getNavigation();
+
+                  if (item) {
+                    navigate(item.href);
+                  }
+                }}
+              >
+                Add recipes
+              </Button>
+            )}
+          </div>
+        </div>
+        {/* /End replace */}
+      </div>
+    );
+  }
 
   return (
     <div className="mx-auto px-4 sm:px-6 md:px-8">
