b14cacab7046483184184aeb30460320e604ba0b
Bram <bram.decuypere@gmail.com>
2023-02-17 21:38:37 +0100

    fix typings

--- imports/ui/hooks/active-list.hook.tsx
+++ imports/ui/hooks/active-list.hook.tsx
@@ -1,21 +1,27 @@
 import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
+import { ActiveList } from "/interfaces/active-list";
 
 const ActiveListHook = () => {
-  return useTracker(() => {
+  return useTracker((): ActiveList & { loading: boolean } => {
     const handler = Meteor.subscribe("users.activeList");
 
     if (!Meteor.user() || !handler.ready()) {
-      return { recipes: [], selectedIngredients: [], loading: true };
+      return {
+        recipes: [],
+        selectedIngredients: [],
+        extraIngredients: [],
+        loading: true,
+      };
     }
 
     const {
-      activeList: { recipes, selectedIngredients },
+      activeList: { recipes, selectedIngredients, extraIngredients },
     } = Meteor.user({
       fields: { activeList: 1 },
     }) as Meteor.User;
 
-    return { recipes, selectedIngredients, loading: false };
+    return { recipes, selectedIngredients, extraIngredients, loading: false };
   });
 };
 
