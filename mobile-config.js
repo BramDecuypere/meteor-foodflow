05900c5193552930dd5f32d5892ea964e132c807
Bram <bram.decuypere@gmail.com>
2023-03-27 21:26:54 +0200

    mobile config

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+    id: 'com.growmyflow.food',
+    name: 'FoodFlow',
+    description: 'Get über power in one button click',
+    author: 'Grow My Flow',
+    email: 'bram@growmyflow.com',
+    website: 'https://food.growmyflow.com'
+  });
