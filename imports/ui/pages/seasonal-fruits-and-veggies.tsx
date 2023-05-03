d91f74b013bc6f30d8b3646eb06426d1f712bb06
Bram <bram.decuypere@gmail.com>
2023-05-03 14:49:14 +0200

    implement current month

--- imports/ui/pages/seasonal-fruits-and-veggies.tsx
+++ imports/ui/pages/seasonal-fruits-and-veggies.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { SEASONAL_FRUITS_AND_VEGETABLES } from "/constants/seasonal-fruits-and-vegetables";
 import { Months } from "/enums/months.enum";
 import { ProduceType } from "/enums/produce-type.enum";
+import { DateTime } from "luxon";
 
 const SeasonalFruitsAndVeggies = () => {
-  const [selectedMonth, setSelectedMonth] = useState(Months.MAY);
+  const currentMonth = DateTime.now().monthLong?.toUpperCase();
+  const [selectedMonth, setSelectedMonth] = useState(currentMonth);
 
   const lists = [
     {
@@ -21,7 +23,24 @@ const SeasonalFruitsAndVeggies = () => {
     <div className="mx-auto flex max-w-xl px-4 sm:px-6 md:px-8">
       <div className="flex flex-col justify-between py-4 px-2 my-4 w-full">
         <div className="text-2xl mb-4">
-          Vegetables of the month: {selectedMonth}
+          Vegetables of the month:
+          <select
+            value={selectedMonth}
+            onChange={(e) => setSelectedMonth(e.target.value)}
+          >
+            <option value={Months.JANUARY}>{Months.JANUARY}</option>
+            <option value={Months.FEBRUARY}>{Months.FEBRUARY}</option>
+            <option value={Months.MARCH}>{Months.MARCH}</option>
+            <option value={Months.APRIL}>{Months.APRIL}</option>
+            <option value={Months.MAY}>{Months.MAY}</option>
+            <option value={Months.JUNE}>{Months.JUNE}</option>
+            <option value={Months.JULY}>{Months.JULY}</option>
+            <option value={Months.AUGUST}>{Months.AUGUST}</option>
+            <option value={Months.SEPTEMBER}>{Months.SEPTEMBER}</option>
+            <option value={Months.OCTOBER}>{Months.OCTOBER}</option>
+            <option value={Months.NOVEMBER}>{Months.NOVEMBER}</option>
+            <option value={Months.DECEMBER}>{Months.DECEMBER}</option>
+          </select>
         </div>
 
         <div className="flex w-full">
