ee422ba67f40109c6f043d7aa7f277cb68f14923
Bram <bram.decuypere@gmail.com>
2023-05-10 15:59:29 +0200

    update groceries

--- imports/ui/pages/groceries.tsx
+++ imports/ui/pages/groceries.tsx
@@ -138,7 +138,7 @@ const Groceries = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 bottom-16 z-10 mt-2 w-56 origin-bottom-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 bottom-16 md:bottom-20 z-10 mt-2 w-56 origin-bottom-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {selectedView === GroceriesListStates.GROCERIES_LIST && (
               <>
                 <div className="py-1">
