8761262493080f33de7a7fbe99fc89a7f61fb0d7
Bram <bram.decuypere@gmail.com>
2023-11-15 18:53:29 +0100

    update recipes

--- imports/ui/pages/recipes.tsx
+++ imports/ui/pages/recipes.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { Meteor } from "meteor/meteor";
+import cn from "classnames";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisVerticalIcon, PlusIcon } from "@heroicons/react/24/outline";
+
 import RecipeListItem from "../molecules/recipe-list-item";
 import UserRecipes from "../hooks/user-recipes.hook";
 import ActiveList from "../hooks/active-list.hook";
 import RecipeList from "../molecules/recipe-list";
 import UserDefaultServings from "../hooks/user-default-servings.hook";
-import { Labels } from "/enums/labels.enum";
 
 const Recipes = () => {
-  const [activeLabels, setActiveLabels] = useState([]);
+  const [activeLabels, _] = useState([]);
   const userRecipes = UserRecipes({ labels: activeLabels });
   const { recipes, loading } = ActiveList();
   const userServings = UserDefaultServings();
@@ -17,6 +20,10 @@ const Recipes = () => {
 
   // This is setting default recipes if there are no recipes found
   Meteor.call("users.defaultRecipes");
+
+  const onOptionsClick = () => {
+    console.log("onoptionsclick");
+  };
 
   const getRecipes = () => {
     return userRecipes.map((recipe, idx) => {
@@ -54,8 +61,65 @@ const Recipes = () => {
         <RecipeList>
           {!loading
             ? getRecipes()
-            : [...Array(10)].map(() => <RecipeListItem loading={loading} />)}
+            : [...Array(10)].map((_, idx) => (
+                <RecipeListItem key={idx} loading={loading} />
+              ))}
         </RecipeList>
+        <Menu
+          as="div"
+          className="inline-block text-left fixed bottom-10 right-5 md:bottom-10 md:right-10 z-50"
+        >
+          <Menu.Button
+            className="cursor-pointer bg-orange rounded-full p-3 shadow-lg"
+            onClick={onOptionsClick}
+          >
+            <EllipsisVerticalIcon className="w-8 h-8 text-4xl text-white" />
+          </Menu.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 bottom-16 md:bottom-20 z-10 mt-2 w-56 origin-bottom-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="py-1">
+                <Menu.Item>
+                  <button
+                    // onClick={() => {
+                    //   setAddModalIsOpen(true);
+                    // }}
+                    className={cn(
+                      "flex px-4 py-2 text-sm w-full text-gray-900"
+                    )}
+                  >
+                    Add new recipe
+                  </button>
+                </Menu.Item>
+              </div>
+
+              {/* <hr /> */}
+
+              {/* <div className="py-1">
+                <Menu.Item>
+                  {() => (
+                    <button
+                      // onClick={resetList}
+                      className={cn(
+                        "flex px-4 py-2 text-sm w-full text-gray-900"
+                      )}
+                    >
+                      Reset list
+                    </button>
+                  )}
+                </Menu.Item>
+              </div> */}
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
     </div>
   );
