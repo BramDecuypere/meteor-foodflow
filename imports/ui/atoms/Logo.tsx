e8c5ee18e9e8e6ed24954afcfe23ebf69e2f8a03
Bram <bram.decuypere@gmail.com>
2024-08-16 14:33:33 +0200

    improve logo

--- imports/ui/atoms/Logo.tsx
+++ imports/ui/atoms/Logo.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Logo = () => {
-  return <div className="font-extrabold text-2xl">FOODFLOW</div>;
+  return (
+    <div className="font-extrabold text-2xl border-primary">
+      <span className="mr-1">👨‍🍳</span>
+      FOOD<span className="text-primary">FLOW</span>
+    </div>
+  );
 };
 
 export default Logo;
